9eccb5f4353c0193eb5fcc3f49a79f4f8aae0f85
Mark Burkut <gulliverfile@gmail.com>
2019-12-27 17:15:22 +0200

    add cron rm ols users

---
+++ cron/ext/clean_old_users.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#
+DIR="/public/kinoinfo/cron"
+source /public/env/bin/activate
+cd /public/kinoinfo
+DX=`date`
+SX=`basename "$0"`
+echo $DX __ $SX >> $DIR/ext/cron.log 2>&1
+/public/env/bin/python  $DIR/clean_old_users.py >> $DIR/ext/cron.log 2>&1
